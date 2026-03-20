27266f2a6fa6df2cd06f49bdf90049d538c4417f
zaibanees33-source <zaibanees@gmail.com>
2026-03-20 09:57:59 +0530

    fixed port

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.post("/contact", async (req, res) => {
         res.send("Failed to send message");
     }
 });
-
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+  const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Server running on port" + PORT);
 });
